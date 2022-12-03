7449db82b46a6bc60f012833739e9959ee9515dc
pumisj <pumisj@naver.com>
2022-12-03 20:48:31 +0900

    edit slack_message.py

--- message/__init__.py
+++
@@ -1,1 +0,0 @@
-from .slack_message import MaskDetector
--- message/slack_message.py
+++ message/slack_message.py
@@ -1,29 +1,28 @@
+# First, make your own Slack app and type in the token
+# Select the channel you want to send the message to
+
 import slack_sdk
 
-SLACK_TOKEN = 'your slack token'
+SLACK_TOKEN = 'your slack bot token'
 SLACK_CHANNEL = '#your slack channel'
 
 class SendSlackMessage:
-
-    #argument : self, detected object
-    def __init__(self, is_detected):
-        self.is_detected = is_detected
-
-    def send_message(self):
-        #slack_token
+    # argument : smile detected object
+    def send_message(list):
+        # slack_token
         slack_token = SLACK_TOKEN
         
-        #slack channel
+        # slack channel
         channel = SLACK_CHANNEL
 
-        #slack_message
-        if self.is_detected:
+        # slack_message
+        if len(list) > 0:
             slack_message = 'Smile Detected'
         else:
             slack_message = 'Smile Not Detected'
 
-        #client
+        # client
         client = slack_sdk.WebClient(token=slack_token)
         
-        #send message
+        # send message
         client.chat_postMessage(channel=channel, text=slack_message)
